e25dd9b7b45e41d23c0e881bc461b2b06e19653d
ваше_имяecho <117764659+Velsovskiy@users.noreply.github.com>
2022-11-12 14:12:17 +0300

    Update Main.java
    Format

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        String command=args[0];
-        String FilePath=args[1];
-        int keyFirst= Integer.parseInt(args[2]);
-        int key=keyFirst%26;
-        if("encode".equals(command)){
+        String command = args[0];
+        String FilePath = args[1];
+        int keyFirst = Integer.parseInt(args[2]);
+        int key = keyFirst % 26;
+        if ("encode".equals(command)) {
             System.out.println("encode");
-            Encoder encoder=new Encoder();
-            encoder.encodeFile(FilePath,key);
-        }else if("decode".equals(command)){
+            Encoder encoder = new Encoder();
+            encoder.encodeFile(FilePath, key);
+        } else if ("decode".equals(command)) {
             System.out.println("decode");
-            Decoder decoder=new Decoder();
-            decoder.decodeFile(FilePath,key);
+            Decoder decoder = new Decoder();
+            decoder.decodeFile(FilePath, key);
         }
-        
+
     }
 
 }
