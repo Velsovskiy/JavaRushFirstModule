ed12808a012517d2bca8ad503be7d5cc510088af
ваше_имяecho <117764659+Velsovskiy@users.noreply.github.com>
2022-11-12 14:12:14 +0300

    Update Encoder.java
    Format

--- Main/src/Encoder.java
+++ Main/src/Encoder.java
@@ -6,50 +6,54 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class Encoder {
-    public final List<Character> ALPHABET=new ArrayList<>(Arrays.asList('a','b','c','d','e','f','g','h','i','k','l','m','n','o','p'
-    ,'q','r','s','t','u','v','w','x','y','z'));
+    public final List<Character> ALPHABET = new ArrayList<>(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p'
+            , 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'));
+    public List<Character> encodedAlphabet = new ArrayList<>(ALPHABET);
 
 
-    public void encodeFile(String FilePath,int key){
-        Path path= Paths.get(FilePath);
+    public void encodeFile(String FilePath, int key) {
+        Path path = Paths.get(FilePath);
         try {
-            String text=Files.readString(path);
-            String encodedText=encode(text,key);
+            String text = Files.readString(path);
+            String encodedText = encode(text, key);
 
-            StringBuilder sb=new StringBuilder(FilePath);
-            sb.insert(FilePath.length()-4,"(encoded)");
-            Path outFilePath=Paths.get(sb.toString());
-            Files.write(outFilePath,encodedText.getBytes(StandardCharsets.UTF_8));
+            StringBuilder sb = new StringBuilder(FilePath);
+            sb.insert(FilePath.length() - 4, "(encoded)");
+            Path outFilePath = Paths.get(sb.toString());
+            Files.write(outFilePath, encodedText.getBytes(StandardCharsets.UTF_8));
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-    public String encode (String text, int key){
-        StringBuilder sb=new StringBuilder();
+
+    public String encode(String text, int key) {
+        StringBuilder sb = new StringBuilder();
 
         for (char c : text.toCharArray()) {
-            sb.append((encode(c,key)));
+            sb.append((encode(c, key)));
         }
         return sb.toString();
     }
-    public Character encode(Character c,int key){
-        boolean LetterCase=false;
-        if(Character.isUpperCase(c)){
-            LetterCase=true;
-            c=Character.toLowerCase(c);
+
+    public Character encode(Character c, int key) {
+        boolean LetterCase = false;
+        if (Character.isUpperCase(c)) {
+            LetterCase = true;
+            c = Character.toLowerCase(c);
         }
 
-        if (ALPHABET.contains(c)){
-            List<Character> encodedAlphabet=new ArrayList<>(ALPHABET);
-            Collections.rotate(encodedAlphabet,key);
-            int index=ALPHABET.indexOf(c);
-            Character result=encodedAlphabet.get(index);
-            if(LetterCase){
-                result=Character.toUpperCase(result);
+        if (ALPHABET.contains(c)) {
+            Collections.rotate(encodedAlphabet, key);
+            int index = ALPHABET.indexOf(c);
+            Character result = encodedAlphabet.get(index);
+            if (LetterCase) {
+                result = Character.toUpperCase(result);
             }
             return result;
-        }else return c;
+        } else {
+            return c;
+        }
 
     }
 }
