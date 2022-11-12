b0ed0caeab3def47229be67d9c06c53cdc3b9db9
ваше_имяecho <117764659+Velsovskiy@users.noreply.github.com>
2022-11-12 14:38:01 +0300

    Update Main.java
    rename

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         String command = args[0];
-        String FilePath = args[1];
-        int keyFirst = Integer.parseInt(args[2]);
-        int key = keyFirst % 26;
+        String filePath = args[1];
+        int key = Integer.parseInt(args[2]);
+        key = key % 26;
         if ("encode".equals(command)) {
             System.out.println("encode");
             Encoder encoder = new Encoder();
-            encoder.encodeFile(FilePath, key);
+            encoder.encodeFile(filePath, key);
         } else if ("decode".equals(command)) {
             System.out.println("decode");
             Decoder decoder = new Decoder();
-            decoder.decodeFile(FilePath, key);
+            decoder.decodeFile(filePath, key);
         }
 
     }
